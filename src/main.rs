0710f52c8d771e965386431f983d4b96176a60ee
Goran Ojkić <goran.ojkic@fyber.com>
2018-03-31 19:07:44 +0200

    introduce fn calls and tuple

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate rand;
+
 use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
@@ -7,7 +8,7 @@ use std::result::Result::Err;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = random_number();
 
     loop {
         println!("Please input your guess!");
@@ -24,6 +25,10 @@ fn main() {
 
         println!("You guessed {}", guess);
 
+        let is_correct = is_guessed(guess, secret_number);
+
+        println!(" {} -> {}", is_correct.0, is_correct.1);
+
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
@@ -32,5 +37,17 @@ fn main() {
                 break;
             }
         }
+    }
+}
+
+fn random_number() -> u32 {
+    rand::thread_rng().gen_range(1, 101)
+}
+
+fn is_guessed(user_number: u32, generated_number: u32) -> (bool, String) {
+    match user_number.cmp(&generated_number) {
+        Ordering::Less => (false, "Too small!".to_string()),
+        Ordering::Greater => (false, "Too big!".to_string()),
+        Ordering::Equal => (true, "You win".to_string())
     }
 }
