fd7e665b253dcfe60e4769de2eeab115200bbada
Goran Ojkić <gojkic@gmail.com>
2019-09-16 20:44:17 +0200

    back at it again

---
+++ variables/src/main.rs
@@ -0,0 +1,15 @@
+
+// Constants can be declared in any scope, 
+//including the global scope, which makes them useful for values that many parts of code need to know about.
+const GOGA_JE_CAR:u32 = 1;
+fn main() {
+    // variables are immutable by default
+    let mut x = 5;
+    x = GOGA_JE_CAR;
+    //variables can be shadowed, and also we can change the type
+    //and keep the name
+    let x = 2;
+    let guess: u32 = "42".parse().expect("not a number baby!");
+    let heart_eyed_cat = '😻';
+    println!("Hello, world! {}", heart_eyed_cat);
+}
