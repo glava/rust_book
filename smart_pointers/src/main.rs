61f6ad9f2e2bfd4136e9f867ebc4d78eb4390484
Goran Ojkić <gojkic@gmail.com>
2019-09-26 22:24:39 +0200

    ref count tutorial

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -2,6 +2,11 @@ enum List {
     Cons(i32, Box<List>),
     Nil,
 }
+use std::rc::Rc;
+enum RcList {
+    Cons(i32, Rc<List>),
+    Nil,
+}
 
 use crate::List::{Cons, Nil};
 
@@ -27,6 +32,18 @@ impl <T> Deref for MyBox<T> {
     }
 }
 
+struct SmartPointer {
+    data: String,
+}
+
+impl Drop for SmartPointer {
+    fn drop(&mut self) {
+        println!("dropping like it is hot");
+    }
+}
+
+
+
 fn main() {
     println!("Hello, world!");
 
@@ -40,6 +57,15 @@ fn main() {
     let x = 5;
     let y = MyBox::new(x);
 
+    let sp = SmartPointer { data: String::from("lolz")};
+    use List::{Cons, Nil};
+    // let graph = Cons(5, Rc::new(
+    //                 Cons(10, Rc::new(Nil))));
+
+    // let pointer1 = Cons(4, Rc::clone(&graph));
+    // let pointer2 = Cons(5, Rc::clone(&graph));
+
+    println!("create");
     assert_eq!(5, x);
     add(&y);
 }
